e890025c9acae019f754a1ad8e5d1742c772fe1d
Jose <Jose@DESKTOP-PLKIIDT>
2021-05-15 05:01:31 +0200

    Formulario realizado

--- ControlVersiones/ControlVersiones/Form1.Designer.cs
+++ ControlVersiones/ControlVersiones/Form1.Designer.cs
@@ -29,20 +29,112 @@ namespace ControlVersiones
         /// </summary>
         private void InitializeComponent()
         {
+            this.tNombre = new System.Windows.Forms.TextBox();
+            this.tApellidos = new System.Windows.Forms.TextBox();
+            this.tTeléfono = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // tNombre
+            // 
+            this.tNombre.Location = new System.Drawing.Point(125, 12);
+            this.tNombre.Name = "tNombre";
+            this.tNombre.Size = new System.Drawing.Size(100, 23);
+            this.tNombre.TabIndex = 0;
+            // 
+            // tApellidos
+            // 
+            this.tApellidos.Location = new System.Drawing.Point(125, 41);
+            this.tApellidos.Name = "tApellidos";
+            this.tApellidos.Size = new System.Drawing.Size(100, 23);
+            this.tApellidos.TabIndex = 1;
+            // 
+            // tTeléfono
+            // 
+            this.tTeléfono.Location = new System.Drawing.Point(125, 70);
+            this.tTeléfono.Name = "tTeléfono";
+            this.tTeléfono.Size = new System.Drawing.Size(100, 23);
+            this.tTeléfono.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Apellidos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Teléfono";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(27, 128);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(198, 105);
+            this.textBox4.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(392, 319);
+            this.ClientSize = new System.Drawing.Size(254, 245);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tTeléfono);
+            this.Controls.Add(this.tApellidos);
+            this.Controls.Add(this.tNombre);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tNombre;
+        private System.Windows.Forms.TextBox tApellidos;
+        private System.Windows.Forms.TextBox tTeléfono;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
     }
 }
 
